f84c11a72e23887ad29a12a950548d0947e3accc
Muzamil-Akbar912 <darmuzamil006@gmail.com>
2026-09-24 20:18:39 +0530

    add optimized approach

--- XpowN.java
+++ XpowN.java
@@ -9,9 +9,28 @@ public class XpowN {
         int xn = x * xnm1;
         return xn;
     }
+
+    // Approach 02 optimized approach O(logn)...
+    public static int pow2(int x, int n) {
+        // base case..
+        if (n == 0) {
+            return 1;
+        }
+        
+        int halfPow = pow2(x, n/2);
+        int halfPowSq = halfPow * halfPow;
+
+        // n == odd...
+        if (n % 2 != 0) {
+            halfPowSq = x * halfPowSq;
+        }
+        // n == even...
+        return halfPowSq;
+    }
     public static void main(String[] args) {
         int x = 2;
         int n = 10;
         System.out.println(pow(x, n));
+        System.out.println(pow2(x, n));
     }
 }
